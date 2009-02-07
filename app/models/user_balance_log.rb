2d5f74bd7336aeaa63e85145bd77fda234af17ad
unknown <rakoth3d@gmail.com>
2009-02-07 19:14:09 +0500

    fit tests

--- app/models/user_balance_log.rb
+++
@@ -1,5 +0,0 @@
-class UserBalanceLog < ActiveRecord::Base
-
-  belongs_to :user
-  
-end
--- test/unit/game_test.rb
+++ test/unit/game_test.rb
@@ -44,36 +44,36 @@ class GameTest < ActiveSupport::TestCase
 
   test "method next_level should do nothing if game in wait status" do
     game = games(:wait)
-    blind_level = game.blind_level
-    game.next_level
-    assert_equal blind_level, game.blind_level
+    assert_no_difference 'game.blind_level' do
+      game.next_level
+    end
   end
 
   test "method next_level should change blind level and next level time if game in start status and its time" do
     game = games(:start)
-    blind_level = game.blind_level
     type = game_types(:one)
     game.next_level_time = Time.now
     game.type = type
-    game.next_level
-    assert_equal blind_level + 1, game.blind_level, "blind_level should be increase by 1"
+    assert_difference 'game.blind_level' do
+      game.next_level
+    end
     assert_in_delta Time.now + type.change_level_time.minutes, game.next_level_time, 1, "time to next level should be set"
   end
 
   test "method next_level should do nothing if start and its not time" do
     game = games(:start)
-    blind_level = game.blind_level
     game.next_level_time = Time.now + 10.minutes
-    game.next_level
-    assert_equal blind_level, game.blind_level
+    assert_no_difference 'game.blind_level' do
+      game.next_level
+    end
   end
 
   test "method next_level should set next_level_time to nil if blind_level is max" do
     game = games(:max_level)
-    blind_level = game.blind_level
     game.next_level_time = Time.now
-    game.next_level
-    assert_equal blind_level, game.blind_level, "blind level must be same"
+    assert_no_difference 'game.blind_level' do
+      game.next_level
+    end
     assert_nil game.next_level_time, "no more levels"
   end
 
